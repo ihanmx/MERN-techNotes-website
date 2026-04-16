58bfaaa03c00c5ebd20bc319f7b53227839842a8
Hanan <Hanan.bayazeed56@gmail.com>
2026-04-16 18:08:36 +0300

    feat(userController)

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const rootRoute = require("./routes/root");
+const userRoutes = require("./routes/useRoutes");
 const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", rootRoute);
+app.use("/users", userRoutes);
 // /* not supported
 app.all("/{*path}", (req, res) => {
   res.status(404);
